17a2cd32ef8cd9a8e5265609b2ffde506f699e35
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2023-02-16 14:49:25 +0100

    navbar changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,43 +2,30 @@ import { Outlet, NavLink } from "react-router-dom";
 
 export function Navbar() {
     return (
-        <>
-            <div>
+        <div>
+            <nav className="navbar">
                 <NavLink to="/">
-                    <div>
-                        <p>Recipe World</p>
-                    </div>
+                    <p>Recipe World</p>
                 </NavLink>
-            </div>
-            <div>
-                <nav className="navbar">
-                    <NavLink to="/beef">
-                    <div>
-                        <p>Beef</p>
-                    </div>
-                    </NavLink>
 
-                    <NavLink to="/chicken">
-                    <div>
-                        <p>Chicken</p>
-                    </div>
-                    </NavLink>
+                <NavLink to="/beef">
+                    <p>Beef</p>
+                </NavLink>
 
-                    <NavLink to="/seafood">
-                    <div>
-                        <p>Seafood</p>
-                    </div>
-                    </NavLink>
+                <NavLink to="/chicken">
+                    <p>Chicken</p>
+                </NavLink>
 
-                    <NavLink to="/veggie">
-                    <div>
-                        <p>Veggie</p>
-                    </div>
-                    </NavLink>
-                </nav>
+                <NavLink to="/seafood">
+                    <p>Seafood</p>
+                </NavLink>
 
-                <Outlet />
-            </div>
-        </>        
+                <NavLink to="/veggie">
+                    <p>Veggie</p>
+                </NavLink>
+            </nav>
+
+            <Outlet />
+        </div>  
     );
 }
